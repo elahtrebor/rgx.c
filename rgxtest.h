594684e73b01beebbb57882034473bf1b0610b6e
elahtrebor <44793384+elahtrebor@users.noreply.github.com>
2024-09-06 23:40:22 -0500

    initial

---
+++ rgxtest.h
@@ -0,0 +1,23 @@
+#ifndef RGXTEST 
+#define RGXTEST
+
+
+char * tokenmap[] = {
+      "NA",
+      "BOL",
+      "EOL",
+      "DOT",
+      "CHAR",
+      "STAR",
+      "CLASS",
+      "PLUS",
+      "MINUS",
+      "RANGE",
+      "BACKSLASH",
+      "SPACE",
+      "END",
+      "ENDPAT"    
+ }; 
+
+#endif
+
